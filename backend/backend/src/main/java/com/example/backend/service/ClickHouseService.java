7c8edaf1c612bb9f9e650f80e85d62cc86f2e087
Biswajit Kabi <biswajitkabi711@gmail.com>
2025-04-16 01:18:13 +0530

    Initialize React app and install axios

--- backend/backend/src/main/java/com/example/backend/service/ClickHouseService.java
+++ backend/backend/src/main/java/com/example/backend/service/ClickHouseService.java
@@ -10,12 +10,14 @@ import java.util.*;
 public class ClickHouseService {
 
     public Connection connect(String host, int port, String db, String user, String token) throws SQLException {
-        String url = String.format("jdbc:clickhouse://%s:%d/%s", host, port, db);
+        String url = String.format("jdbc:clickhouse://%s:%d/%s?ssl=false", host, port, db);
+
 
         Properties props = new Properties();
         props.setProperty("user", user);
         props.setProperty("password", token);  // JWT passed as password
-        props.setProperty("ssl", "true");
+        props.setProperty("ssl", "false");
+        props.setProperty("compress", "0");
 
         return DriverManager.getConnection(url, props);
     }
